4657d0fab9913dec943e96fec3feb2d47d7732a3
David Franz <davidfranznz@gmail.com>
2021-05-16 03:41:47 +1200

    implemented all probability functions

--- algorithm.py
+++ algorithm.py
@@ -6,24 +6,38 @@ import math
 from dataloader import DataLoader
 
 # not_spam_counts (classified_as_spam = false) at index 0 of tuple, spam_counts (classified_as_spam = true) at index 1
-word_counts__classified_as_spam = (not_spam_counts, spam_counts) = DataLoader.load_data_as_counts()
+word_counts__classified_as_spam =
+	(not_spam_counts, spam_counts, probability_of_spam) = 
+		DataLoader.load_data_as_counts()
+
+def probability_of_spam():
+	return word_counts__classified_as_spam[-1]
+
+def probability_of_not_spam():
+	return 1 - probability_of_spam()
 
 def prior_probability_of_word(index_of_word):
-	pass
+	return ((word_counts__classified_as_spam[False][index_of_word][1] +
+			 word_counts__classified_as_spam[True][index_of_word][1]) /
+	   			(word_counts__classified_as_spam[False][index_of_word][0] +
+			 	  word_counts__classified_as_spam[True][index_of_word][0] + 
+			     word_counts__classified_as_spam[False][index_of_word][1] + 
+				  word_counts__classified_as_spam[True][index_of_word][1]))
+
+def prior_probability_of_not_word(index_of_word):
+	return 1 - prior_probability_of_word(index_of_word)
 
 # this returns the probability that an email contains a certain word for a given email class
 # class meaning classified as spam (true) or not (false)
 def posterior_probability_of_word_given_class(index_of_word, classified_as_spam): # posterior probability (given class)
 	assert type(classified_as_spam) is bool
 
-	print(classified_as_spam)
-	print(int(classified_as_spam))
-
 	return (word_counts__classified_as_spam[int(classified_as_spam)][index_of_word][1] / 
-		(word_counts__classified_as_spam[int(classified_as_spam)][index_of_word][0] + word_counts__classified_as_spam[int(classified_as_spam)][index_of_word][1]))
+				(word_counts__classified_as_spam[int(classified_as_spam)][index_of_word][0] + 
+				 word_counts__classified_as_spam[int(classified_as_spam)][index_of_word][1]))
 
 # this returns the probability that an email doesn't contain a certain word for a given email class
-def not_posterior_probability_of_word_given_class(index_of_word, classified_as_spam): # posterior probability (given class
+def posterior_probability_of_not_word_given_class(index_of_word, classified_as_spam): # posterior probability (given class
 	return 1 - posterior_probability_of_word_given_class(index_of_word, classified_as_spam)
 
 if __name__ == "__main__":
@@ -40,4 +54,9 @@ if __name__ == "__main__":
 	print(word_counts__classified_as_spam[False])
 
 	print(posterior_probability_of_word_given_class(0, True))
-	print(not_posterior_probability_of_word_given_class(0, True))
+	print(posterior_probability_of_not_word_given_class(0, True))
+
+	print(prior_probability_of_word(0))
+	print(prior_probability_of_not_word(0))
+
+	
--- dataloader.py
+++ dataloader.py
@@ -2,13 +2,16 @@
 # ith index of list is the ith word of our list of spam words:
 # 		• index 0 of that list is the count of emails that DON'T have that word (false)
 # 		• index 1 of that list is the count of emails that DO have that word (true)
-not_spam_counts = [[1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1]]
-spam_counts = [[1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1]]
+not_spam_word_counts = [[1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1]]
+spam_word_counts = [[1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1]]
 
 class DataLoader:
 
 	@staticmethod
 	def load_data_as_counts():
+		number_of_spam_emails = 0
+		number_of_not_spam_emails = 0
+
 		with open("spamLabelled.dat") as f: # this will automatically close the file f when we exit this block
 			lines = ["".join(line.split()) for line in f.readlines()]
 
@@ -16,17 +19,19 @@ class DataLoader:
 				line_is_spam = bool(int(line[len(line)-1]))
 
 				if line_is_spam:
+					number_of_spam_emails += 1
 					# character will be 0 or 1 to indicate whether that word is in the email
 					for index, character in enumerate(line[:-1]):
-						spam_counts[index][int(character)] += 1
+						spam_word_counts[index][int(character)] += 1
 
 				else: # this means that the document has been classified as not spam
+					number_of_not_spam_emails += 1
 					# character will be 0 or 1 to indicate whether that word is in the email
 					for index, character in enumerate(line[:-1]):
-						not_spam_counts[index][int(character)] += 1
+						not_spam_word_counts[index][int(character)] += 1
 
 
 		# we return a tuple of the lists described above with:
 		# 		• index 0 is the list of counts where the email ISN'T marked as spam (false)
 		# 		• index 1 is the list of counts where the email IS marked as spam (true)
-		return not_spam_counts, spam_counts
+		return not_spam_counts, spam_counts, (number_of_spam_emails / (number_of_spam_emails + number_of_not_spam_emails))
